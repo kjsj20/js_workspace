a4664d0b0cefcf27d048b407cbeccbccccdb1666
DESKTOP-FK0MJUV\JS <kjsj20@naver.com>
2020-09-22 18:57:10 +0900

    과제 끝!

--- project0922/순환배너.js
+++ project0922/순환배너.js
@@ -1,30 +1,45 @@
 var wrap;
-var divArray = new Array(5); //반복문으로 채워넣기 위해
+var arr = new Array(5); //반복문으로 채워넣기 위해
+var targetX = 0;
+var a = 0.2;
 
 window.addEventListener("load", function () {
     init();
+    setInterval("setXY()", 20);
 });
 
 function init() {
     wrap = document.querySelector("#wrap");
 
-    for (i = 1; i <= 5; i++) {
-        var div = document.createElement("div");
-        divArray[i]=div;
-        // div.src = "../images/travel/" + i + ".jpg"
-        div.style.width = 100 + "px";
-        div.style.height = 75 + "px";
-        div.style.position = "absolute";
-        div.style.left = ((i - 1) * 100) + "px";
-        div.style.border = "1px solid red";
-        div.style.boxSizing = "border-box";
-        wrap.appendChild(divArray[i]);
+    for (i = 0; i < arr.length; i++) {
+        var img = document.createElement("img");
+        arr[i] = img;
+        img.src = "../images/travel/" + (i + 1) + ".jpg"
+        console.log(img.src);
+        img.style.width = 100 + "px";
+        img.style.height = 75 + "px";
+        img.style.position = "absolute";
+        img.style.left = (i * 100) + "px";
+        // img.style.border = "1px solid red";
+        img.style.boxSizing = "border-box";
+        wrap.appendChild(img);
     }
+    
 }
 
 //이미지들의 좌표를 좌측방향으로 (x음수) 이동하다가
-function move() {
-    for(var i =0; i<divArray.length;i++){
-        divArray[i].style.left = parseInt(divArray[i].style.left) - 10 + "px";
+// function move() {
+//     for (var i = 0; i < arr.length; i++) {
+//         // console.log(divArray.length);
+//         targetX = parseFloat(arr[i].style.left) - 100 + "px";
+//     }
+// }
+
+function setXY(){
+    for (var i = 0; i < arr.length; i++) {
+        arr[i].style.left = parseFloat(arr[i].style.left) + a * ((parseFloat(arr[i].style.left) - 10) - parseFloat(arr[i].style.left)) + "px";
+        if(parseFloat(arr[i].style.left) === -100) {
+            arr[i].style.left = 400 + "px";
+        }
     }
 }
